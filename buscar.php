09d28a8d8b3c206d7c60ab7e286f78c2ac714ceb
unhueso <52423612+unhueso@users.noreply.github.com>
2019-07-01 14:11:51 -0400

    Update buscar.php

--- buscar.php
+++ buscar.php
@@ -1,7 +1,7 @@
 <?php
 	$servername = "localhost";
     $username = "root";
-  	$password = "Tsistema073";
+  	$password = "Password";
   	$dbname = "fallasfar";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
